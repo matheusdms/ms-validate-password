62f5cddd95714bfcb52e72c86d6560a405e27af7
Matheus das Mercês <labsadministrator@matheuszerrambp.lan>
2020-05-14 13:20:02 -0300

    Set lambda function to private and modify first handler

--- src/handlers/validatePassword.js
+++ src/handlers/validatePassword.js
@@ -1,18 +1,10 @@
 'use strict';
 
-module.exports.hello = async event => {
+const handler = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: true,
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+module.exports = { handler };
