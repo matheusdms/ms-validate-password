16043b319f5e5d48c8c3ba21dbd300b98b87a431
Matheus das Mercês <labsadministrator@matheuszerrambp.lan>
2020-05-14 14:07:55 -0300

    Add password rules interface and get handler done

--- src/handlers/validatePassword.js
+++ src/handlers/validatePassword.js
@@ -1,10 +1,38 @@
 'use strict';
 
+const passwordRules = require('../helpers/password-rules');
+
 const handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: true,
-  };
+
+  const requestBody = JSON.parse(event.body);
+
+  try {
+
+    if(!requestBody || !requestBody.password) {
+      throw new Error("Body parameter password is required.");
+    }
+
+    const { password } = requestBody;
+
+    let allRulesOk = true;
+    for (let rule of Object.keys(passwordRules)) {
+      allRulesOk = await passwordRules[rule](password);
+  
+      if(!allRulesOk) break;
+    }
+  
+    return {
+      statusCode: 200,
+      body: allRulesOk,
+    };
+
+  } catch(e) {
+
+    return {
+      statusCode: 500,
+      body: e.message,
+    };
+  }
 };
 
 module.exports = { handler };
---
+++ src/helpers/password-rules.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const validateString = (string) => {
+  return true;
+}
+
+const verifyMinLength = (string) => {
+  return true;
+}
+
+const verifyUpperCaseLetter = (string) => {
+  return true;
+}
+
+const verifyLowerCaseLetter = (string) => {
+  return true;
+}
+
+const verifyEspecialCharacter = (string) => {
+  return true;
+}
+
+const verifyRepeatedCharacter = (string) => {
+  return true;
+}
+
+module.exports = {
+  validateString,
+  verifyMinLength,
+  verifyUpperCaseLetter,
+  verifyLowerCaseLetter,
+  verifyEspecialCharacter,
+  verifyRepeatedCharacter,
+}
